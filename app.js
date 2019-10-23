de9f3bddbba07827877c9d20306c8942c5571526
heidemannleon <leon.heidemann@khk.ee>
2019-10-23 16:24:44 +0300

    nüüd on meil konsooli kasutamise võimalused

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-// Andmetüübid
+// logid konsoolile
 
-// täisarvud
-console.log(10);
-console.log(-4);
-console.log(0);
+// logid
+console.log('123');
 
-// reaalarvud - ujuvkomaarvud
-console.log(2.5);
-console.log(-7.23);
-console.log(-2.0);
-
-// sõned - tekst
-console.log('Hello World');
-
-// tõeväärtused
-console.log(true); // tõene
-console.log(false); // väär
-
-// nimekiri 
-console.log([1, 2, 3, 4]);
-
-// objektid
-console.log({a : 1, b : 2});
+// table
 console.table({a : 1, b : 2});
+
+// veateade
+console.error('See on mingi viga');
+
+// märkused
+console.warn('See on märkus');
+
+// ajakulu kontroll
+console.time('Test')
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+    console.log('Mingi tegevus');
+console.timeEnd('Test');
+
+// puhasta ekraan
+console.clear();
+
