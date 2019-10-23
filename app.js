402ac25610ffccb614652b09d03153771981dde0
heidemannleon <leon.heidemann@khk.ee>
2019-10-23 16:18:07 +0300

    See on osa mis tegeleb andmetüüpidega

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+// Andmetüübid
 
+// täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
 
+// reaalarvud - ujuvkomaarvud
+console.log(2.5);
+console.log(-7.23);
+console.log(-2.0);
 
-console.log("JS test");
+// sõned - tekst
+console.log('Hello World');
+
+// tõeväärtused
+console.log(true); // tõene
+console.log(false); // väär
+
+// nimekiri 
+console.log([1, 2, 3, 4]);
+
+// objektid
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});
