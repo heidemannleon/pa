3d5bf379aec31d0b9d74a8fb8a8c35ea54bf5446
heidemannleon <leon.heidemann@khk.ee>
2019-10-28 11:34:02 +0200

    sort for tsükli abil

--- app.js
+++ app.js
@@ -55,8 +55,25 @@ console.log(arvud);
 
 // for tsükkel
 // üldtsükkel
-let autod = ['Kia', 'Toyota', 'Ford', 'Nissan']; // kia index 0, toyota index 1 jne
-// väljastus
-for(let i = 0; i < autod.length; i++){
-    console.log(autod[i]);
+let numbrid = [];
+// lisame nimekirja sisse juhuslikud arvud
+for(let i = 0; i < 10; i++){
+  let arv = Math.floor(Math.random() * 10);
+  numbrid.push(arv);  
+}
+// kontrollime tulemus
+console.log(numbrid);
+// sorteerime kasvujärjekorras
+// numbrid.sort();
+for(let kord = 0; kord < numbrid.length; kord++){
+  for(let i = 0; i < numbrid.length; i++){
+    let abi;
+    if(numbrid[i] > numbrid[i+1]){
+      abi = numbrid[i];
+      numbrid[i] = numbrid[i+1];
+      numbrid[i+1] = abi;
+    } 
   }
+}
+// kontrollime tulemus
+console.log(numbrid);
